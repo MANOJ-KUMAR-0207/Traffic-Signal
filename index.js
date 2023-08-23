d731a25de6bc311961b1283440f1ea2bab6bd45f
Manoj Kumar G <manoj.kumar@moonraft.com>
2023-08-23 11:57:44 +0530

    Pressed Changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function off()
 {
     document.getElementById("red").classList.remove('redSelected');
@@ -8,28 +7,36 @@ function off()
 function red()
 {
     off();
-    document.getElementById("brake").classList.add("warning");
+    var brake=document.getElementById("brake");
     document.getElementById("red").classList.add("redSelected");
     var accelerator=document.getElementById("accelerator");
     accelerator.classList.remove("go");
     content.innerHTML="Stop !!";
+    if(!brake.classList.contains("pressed"))
+        brake.classList.add("warning");
     if(accelerator.classList.contains("pressed"))
         accelerator.classList.add("warning");
 }
 function yellow()
 {
     off();
+    var brake=document.getElementById("brake");
     document.getElementById("yellow").classList.add("yellowSelected");
-    document.getElementById("brake").classList.add("warning");
     document.getElementById("accelerator").classList.remove("go");
     document.getElementById("content").innerHTML="Wait";
+    if(!brake.classList.contains("pressed"))
+        brake.classList.add("warning");
+    if(accelerator.classList.contains("pressed"))
+        accelerator.classList.add("warning");
 }
 function green()
 {
     off();
+    var accelerator=document.getElementById("accelerator");
     document.getElementById("content").innerHTML="Go !";
     document.getElementById("green").classList.add("greenSelected");
-    document.getElementById("accelerator").classList.add("go");
+    if(!accelerator.classList.contains("pressed"))
+        document.getElementById("accelerator").classList.add("go");
     document.getElementById("brake").classList.remove("warning");
 }
 function stop()
@@ -37,6 +44,7 @@ function stop()
     var brake=document.getElementById("brake");
     brake.classList.remove("warning");
     brake.classList.toggle("pressed");
+    brake.classList.toggle("control");
 }
 function go()
 {
@@ -44,4 +52,5 @@ function go()
     accelerator.classList.remove("go");
     accelerator.classList.remove("warning");
     accelerator.classList.toggle("pressed");
+    accelerator.classList.toggle("control");
 }
